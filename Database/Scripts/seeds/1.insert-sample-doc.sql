cb5bb520842c5c462e3581423fbc6a2e6cf07725
Rafael Bezerra <rafaelbezerra195@gmail.com>
2024-02-28 10:59:21 -0300

    fix document insert

--- Database/Scripts/seeds/1.insert-sample-doc.sql
+++ Database/Scripts/seeds/1.insert-sample-doc.sql
@@ -27,15 +27,16 @@ BEGIN TRANSACTION;
     DECLARE @request_origin VARCHAR(3) = 'SAP';
 
     INSERT INTO dbo.BMA_REQUEST_SCHEMA(Type, Origin, Name, Display, Active, AllowComments, ApprovalRuleId, CreateDate, UpdateDate) 
-    values (@request_type, @request_origin, 'Pedido de Compra', false, false, 1, GETDATE(), GETDATE());
+    values (@request_type, @request_origin, 'purchaseOrder', 'Pedido de Compra', 0, 0, 1, GETDATE(), GETDATE());
 
     DECLARE @request_schema_id INT = 0;
     SELECT @request_schema_id = id from dbo.BMA_REQUEST_SCHEMA where Type = @request_type and Origin = @request_origin;
 
     -- INSERT TAB_SCHEMA
     INSERT INTO dbo.BMA_TAB_SCHEMA(RequestSchemaId, Name, Display, [Order], CreateDate, UpdateDate) 
-    values (@request_schema_id, 'timeline', 'Timeline', true, 1, GETDATE(), GETDATE()),
-           (@request_schema_id, 'itens', "Itens", true, 2, GETDATE(), GETDATE());
+    values (@request_schema_id, 'timeline', 'Timeline', 1, GETDATE(), GETDATE()),
+           (@request_schema_id, 'itens', "Itens", 2, GETDATE(), GETDATE()),
+           (@request_schema_id, 'root', "Root", 0, GETDATE(), GETDATE());
 
     DECLARE @tab_schema_timeline_id INT = 0;
     SELECT @tab_schema_timeline_id = id from dbo.BMA_TAB_SCHEMA where Name = 'timeline'
@@ -43,13 +44,16 @@ BEGIN TRANSACTION;
     DECLARE @tab_schema_itens_id INT = 0;
     SELECT @tab_schema_itens_id = id from dbo.BMA_TAB_SCHEMA where Name = 'itens'
 
+    DECLARE @tab_schema_root_id INT = 0;
+    SELECT @tab_schema_root_id = id from dbo.BMA_TAB_SCHEMA where Name = 'root'
+
     -- INSERT FIELDS_SCHEMA
     INSERT INTO dbo.BMA_FIELD_SCHEMA(RequestSchemaId, Name, TabSchemaId, FieldSchemaTypesId, Required, Display, Visible, [Order], CreateDate, UpdateDate)
-    values (@request_schema_id, 'type', null,  @type_string, true, "Type", true, 1, GETDATE(), GETDATE()),
-           (@request_schema_id, 'value', null,  @type_currency, true, "Value", true, 2, GETDATE(), GETDATE()),
-           (@request_schema_id, 'requester', null,  @type_string, true, "Requester", true, 3, GETDATE(), GETDATE()),
-           (@request_schema_id, 'status', null,  @type_string, true, "Status", false, 4, GETDATE(), GETDATE()),
-           (@request_schema_id, 'material', @tab_schema_itens_id,  @type_string, false, "Material", true, 4, GETDATE(), GETDATE()),
-           (@request_schema_id, 'description', @tab_schema_timeline_id,  @type_string, false, "Description", true, 5, GETDATE(), GETDATE());
+    values (@request_schema_id, 'type', @tab_schema_root_id,  @type_string, 1, "Type", 1, 1, GETDATE(), GETDATE()),
+           (@request_schema_id, 'value', @tab_schema_root_id,  @type_currency, 1, "Value", 1, 2, GETDATE(), GETDATE()),
+           (@request_schema_id, 'requester', @tab_schema_root_id,  @type_string, 1, "Requester", 1, 3, GETDATE(), GETDATE()),
+           (@request_schema_id, 'status', @tab_schema_root_id,  @type_string, 1, "Status", 0, 4, GETDATE(), GETDATE()),
+           (@request_schema_id, 'material', @tab_schema_itens_id,  @type_string, 0, "Material", 1, 4, GETDATE(), GETDATE()),
+           (@request_schema_id, 'description', @tab_schema_timeline_id,  @type_string, 0, "Description", 1, 5, GETDATE(), GETDATE());
 
 COMMIT TRANSACTION;
