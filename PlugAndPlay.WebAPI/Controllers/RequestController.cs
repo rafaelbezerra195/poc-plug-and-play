387e2fd57ac83b63ebd52d03675ecf498b2a6754
Rafael Bezerra <rafaelbezerra195@gmail.com>
2024-02-29 22:34:58 -0300

    add db connection

--- PlugAndPlay.WebAPI/Controllers/RequestController.cs
+++ PlugAndPlay.WebAPI/Controllers/RequestController.cs
@@ -1,5 +1,7 @@
 using System.Text.Json.Nodes;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace PlugAndPlay.WebAPI.Controllers;
 
@@ -8,15 +10,20 @@ namespace PlugAndPlay.WebAPI.Controllers;
 public class RequestController : ControllerBase
 {
     private readonly ILogger<RequestController> _logger;
-
-    public RequestController(ILogger<RequestController> logger)
+    private readonly DbContext _context;
+    
+    public RequestController(ILogger<RequestController> logger, DbContext context)
     {
         _logger = logger;
+        _context = context;
     }
     
     [HttpPost(Name = "Request")]
     public async Task<IActionResult> Post([FromBody] JsonObject body)
     {
+        var connection = _context.Database.GetDbConnection();
+        var results = connection.Query("SELECT * FROM BMA_APPROVAL_RULES");
+        
         return Ok(body["DocumentType"]);
     }
 }
--- PlugAndPlay.WebAPI/Program.cs
+++ PlugAndPlay.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("PlugAndPlayContext")));
 
 var app = builder.Build();
 
